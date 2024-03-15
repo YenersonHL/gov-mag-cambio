6891e194872851b0c74a3b352f74bf12454c23a1
YenersonHL <yenerson.herlop@outlook.com>
2024-03-15 17:01:48 -0500

    contact- responsivo

--- resources/views/components/section-contact.blade.php
+++ resources/views/components/section-contact.blade.php
@@ -1,24 +1,24 @@
-<section class="container-fluid position-relative mb-5 pb-5">
+<section class="container-fluid position-relative mb-5 pb-2">
     <div class="row">
         <div class="col-12 p-0">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-6 bg-img-cover position-relative" style="background-image: url('{{ asset('img/padd/110.jpg') }}');">
+                    <div class="col-12 col-lg-6 bg-img-cover position-relative" style="background-image: url('{{ asset('img/padd/110.jpg') }}');">
                         <div class="position-absolute w-100 h-100 top-0 start-0 bg-orange-mid-gradient"></div>
                         <div class="row align-items-end position-relative h-100 z-1 text-center">
-                            <div class="pb-5">
+                            <div class="py-5">
                                 <img src="{{ asset('img/logo-cambio-white.png') }}" width="200" alt="">
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 bg-orange-gradient text-white">
+                    <div class="col-12 col-lg-6 bg-orange-gradient text-white">
                         <div class="block p-5 my-5">
                             <form class="row form-contat">
-                                <div class="col-9 mb-4">
+                                <div class="col-12 col-xl-9 mb-4">
                                     <h2 class="text-uppercase text-center fw-bold border-bottom pb-1 border-2 border-white">Comentanos tus inquietudes</h2>
                                     <div class="fs-4 lh-sm">Envianos tus sugerencias y preguntas. Para nosotros es importante tu opinio.</div>
                                 </div>
-                                <div class="col-9">
+                                <div class="col-12 col-xl-9">
                                     <div class="row">
                                         <div class="col-6 mb-3">
                                             <label for="input-uno" class="form-label">Nombre</label>
