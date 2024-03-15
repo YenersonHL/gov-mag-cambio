8ae4857a5245a14006ab44f2cc8a4c848a767e57
YenersonHL <yenerson.herlop@outlook.com>
2024-03-15 17:07:20 -0500

    blog

--- resources/views/components/section-blogs.blade.php
+++ resources/views/components/section-blogs.blade.php
@@ -9,16 +9,16 @@
                         subtitle="Así cambio"
                         title="Sanata Marta" />
                     </div>
-                    <div class="col-6">
+                    <div class="col-lg-6 mb-3 mb-lg-0">
                         <x-title-section-blue
                         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nemo deserunt ducimus, qui, tempora velit odio hic aut aliquam cum beatae, at mollitia, aspernatur excepturi ut eligendi cupiditate et pariatur." />
                     </div>
-                    <div class="col-6 ps-4">
+                    <div class="col-lg-6 ps-lg-4">
                         <div class="row">
-                            <div class="col-6">
+                            <div class="col-12 col-md-6 mb-3 mb-md-0">
                                 <x-img-titles img="a-1.jpg" title="Fuerza del cambio" />
                             </div>
-                            <div class="col-6">
+                            <div class="col-12 col-md-6">
                                 <x-img-titles img="a-2.jpg" title="El cambio sigue" />
                             </div>
                         </div>
